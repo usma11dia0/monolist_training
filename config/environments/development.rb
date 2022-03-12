a54ba85341dfea1bc9c5f919dfb9149d6ee8fb15
usma11dia0 <usma11dia0@outlook.com>
2022-03-12 14:36:49 +0000

    add dotenv and whitelist

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "3b3de1a990fc41fd9c23c4ccbf8f13b1.vfs.cloud9.us-east-1.amazonaws.com"
+  
 end
